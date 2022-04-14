04bbf3c616876dd6e7a11ba49afecf06b539ac68
madmax7896 <madmax78968@gmail.com>
2022-04-14 13:24:45 +0000

    munch: Build QTI health HIDL

--- device.mk
+++ device.mk
@@ -86,6 +86,11 @@ PRODUCT_COPY_FILES += \
 # GPS
 LOC_HIDL_VERSION := 4.0
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-service
+
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
