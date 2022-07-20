f60159bc6dbc0967ae30b68164d9d0bd31225996
Bowgo Tsai <bowgotsai@google.com>
2022-07-20 12:57:54 +0530

    munch: Add GSI keys

--- device.mk
+++ device.mk
@@ -90,6 +90,9 @@ PRODUCT_COPY_FILES += \
 # GPS
 LOC_HIDL_VERSION := 4.0
 
+# GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
