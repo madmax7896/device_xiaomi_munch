954432eeb434e3c0f826b1c3f2f67c1023785947
madmax7896 <madmax78968@gmail.com>
2022-07-20 14:49:48 +0530

    munch: Import HIDL manifest from LA.UM.9.12.r1-14300.01-SMxx50.QSSI12.0
    * Add xiaomi specific VINTF entries to a separate manifest file.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,15 @@ TARGET_NO_RAW10_CUSTOM_FORMAT := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/xiaomi_vendor_framework_compatibility_matrix.xml
+
+DEVICE_MANIFEST_FILE += \
+    $(DEVICE_PATH)/configs/hidl/manifest.xml \
+    $(DEVICE_PATH)/configs/hidl/xiaomi_manifest.xml
+
+DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
+
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x00000000
