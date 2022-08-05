5390b13d58e13cc1b891c0322882eb33ce04e32c
madmax7896 <madmax78968@gmail.com>
2022-08-06 01:32:22 +0530

    munch: Update bluetooth blobs from LA.UM.9.12.r1-14100.02-SMxx50.QSSI12.0

--- device.mk
+++ device.mk
@@ -53,8 +53,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Boot
 PRODUCT_PACKAGES += \
