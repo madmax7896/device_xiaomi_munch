49b0065f40c52e49925f9b0375a64170d71f2470
madmax7896 <madmax78968@gmail.com>
2022-11-02 12:26:21 +0000

    munch: configs: wifi: Update WCNSS config from V13.0.5.0.SLMCNXM
    * Also drop WPA supplicant config.

--- device.mk
+++ device.mk
@@ -389,5 +389,4 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
-    $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
