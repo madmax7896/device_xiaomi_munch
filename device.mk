ff81e001e74196a4e54c16700394bc9762c66b21
madmax7896 <madmax78968@gmail.com>
2023-01-14 11:05:43 +0530

    munch: configs: Drop fpc and goodix fingerprint keylayouts
    * We have already disabled the whole event reporting for goodix and also implemented a keyhandler for fpc in kernel.

--- device.mk
+++ device.mk
@@ -247,7 +247,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
 # Keymaster
 PRODUCT_PACKAGES += \
