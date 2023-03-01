e76c6f35a451794f278592974aae290fc6bbf6ac
Adithya <gh0strider.2k18.reborn@gmail.com>
2023-03-01 13:20:22 +0530

    munch: Remove common compatibility matrix inclusion
    * It's moved to qcom/common makefile instead.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,8 +68,6 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/hidl/manifest.xml \
     $(DEVICE_PATH)/configs/hidl/xiaomi_manifest.xml
 
-DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
-
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_munch
 TARGET_RECOVERY_DEVICE_MODULES := libinit_munch
